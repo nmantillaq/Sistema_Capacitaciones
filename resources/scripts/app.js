afc1b6adcfb99b6fceb7d8819a17323eaddeaf27
nmantillaqCertus <90099252+nmantillaqCertus@users.noreply.github.com>
2023-11-14 09:18:20 -0500

    update 20231114

---
+++ resources/scripts/app.js
@@ -0,0 +1,55 @@
+let Lista_Programas = [
+    {
+        codigo: "P001",
+        titulo: "Programa 01",
+        duracion: function(){
+            return this.nroCursos.length * 8 +" h"
+        },
+        nroCursos: ["C001"],
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste necessitatibus id recusandae commodi doloribus nam nesciunt, quae sunt dolores",
+        uri_imagen: "https://i.blogs.es/0cafeb/naruto/840_560.jpeg"
+    }
+    ,
+    {
+        codigo: "P002",
+        titulo: "Programa 02",
+        duracion: function(){
+            return this.nroCursos.length * 8 +" h"
+        },
+        nroCursos: ["C001", "C004"],
+        descripcion: "Lorem del programa 02 ipsum dolor sit amet consectetur adipisicing elit. Illum iste necessitatibus id recusandae commodi doloribus nam nesciunt, quae sunt dolores",
+        uri_imagen: "https://i.blogs.es/0cafeb/naruto/840_560.jpeg"
+    },
+    {
+        codigo: "P002",
+        titulo: "Programa 02",
+        duracion: function(){
+            return this.nroCursos.length * 8 +" h"
+        },
+        nroCursos: ["C001", "C004"],
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste necessitatibus id recusandae commodi doloribus nam nesciunt, quae sunt dolores",
+        uri_imagen: "https://i.blogs.es/0cafeb/naruto/840_560.jpeg"
+    },
+    {
+        codigo: "P003",
+        titulo: "Programa 03",
+        duracion: function(){
+            return this.nroCursos.length * 8 +" h"
+        },
+        nroCursos: ["C001", "C002", "C003"],
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste necessitatibus id recusandae commodi doloribus nam nesciunt, quae sunt dolores",
+        uri_imagen: "https://i.blogs.es/0cafeb/naruto/840_560.jpeg"
+    },
+    {
+        codigo: "P004",
+        titulo: "Programa 04",
+        duracion: function(){
+            return this.nroCursos.length * 8 +" h"
+        },
+        nroCursos: ["C001","C002", "C003", "C005"],
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste necessitatibus id recusandae commodi doloribus nam nesciunt, quae sunt dolores",
+        uri_imagen: "https://i.blogs.es/0cafeb/naruto/840_560.jpeg"
+    }
+
+
+];
---
+++ resources/scripts/programa.js
@@ -0,0 +1,16 @@
+$(document).ready(()=>{
+    obtenerParametro();
+});
+
+function obtenerParametro(){
+    const codigo_Recibido = window.location.search.split("=")[1];
+    const programa = Lista_Programas.find(p => p.codigo ==  atob(codigo_Recibido));
+    generarInformacion(programa);    
+}
+
+
+function generarInformacion(p){
+    $("#id_titulo").text(p.titulo);
+    $("#id_descripcion").text(p.descripcion);
+
+}
